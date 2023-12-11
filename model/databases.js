170a8cf45ff413927c35f31cfd900ba7f8ae4232
jaildesfarias <131688620+jaildesfarias@users.noreply.github.com>
2023-12-11 14:11:49 -0300

    Update databases.js

--- model/databases.js
+++ model/databases.js
@@ -1,7 +1,7 @@
 // Arquivo: database.js
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('consultando', 'root', 'casa', {
+const db = new Sequelize('consultando', 'root', 'fofo', {
     host: 'localhost',
     dialect: 'mysql', // Altere para o seu banco de dados (por exemplo, 'mysql', 'postgres', 'sqlite', etc.)
 });
